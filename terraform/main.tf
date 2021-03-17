00abb836d3a3ac76955c37f886a7f70f43551baa
Eric Bragas <ericbragas412@gmail.com>
2021-03-16 19:34:03 -0700

    gcs bucket and notification

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,3 +29,33 @@ resource "google_cloud_tasks_queue" "default_queue" {
     google_project_service.cloud_tasks_api
   ]
 }
+
+resource "google_storage_bucket" "outgoing_bucket" {
+  name          = "${var.project_id}-outgoing"
+  location      = "US"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_notification" "notification" {
+  bucket         = google_storage_bucket.outgoing_bucket.name
+  payload_format = "JSON_API_V1"
+  topic          = google_pubsub_topic.new_outgoing_topic.id
+  event_types    = ["OBJECT_FINALIZE"]
+  depends_on = [google_pubsub_topic_iam_binding.binding]
+}
+
+resource "google_pubsub_topic" "new_outgoing_topic" {
+  name = "new-outgoing"
+}
+
+// Enable notifications by giving the correct IAM permission to the unique service account.
+data "google_storage_project_service_account" "gcs_account" {
+}
+
+resource "google_pubsub_topic_iam_binding" "binding" {
+  topic   = google_pubsub_topic.new_outgoing_topic.id
+  role    = "roles/pubsub.publisher"
+  members = ["serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"]
+}
